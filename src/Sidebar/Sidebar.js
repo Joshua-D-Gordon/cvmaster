5d2e2edb29e575ab1df1a09cac2c3422c557e0f1
Joshua gordon <jodogo9897@gmail.com>
2024-06-17 00:10:06 +0300

    Sidebar slider 	modified:   public/index.html 	modified:   src/Sidebar/Sidebar.js 	modified:   src/Sidebar/styles.css

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,20 +1,51 @@
-// src/Sidebar.js
-import React from 'react';
+// src/Sidebar/Sidebar.js
+import React, { useState } from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-logo">CV Master</div>
+    <div className={`sidebar ${isCollapsed ? 'collapsed' : ''}`}>
+      <div className="sidebar-logo">
+        {isCollapsed ? "CV" : "CV Master"}
+        <button className="toggle-button" onClick={toggleSidebar}>
+          <i className={`fas ${isCollapsed ? 'fa-arrow-right' : 'fa-arrow-left'}`}></i>
+        </button>
+      </div>
+      <div className="profile-card">
+        <div className="profile-pic"></div>
+        <div className="profile-card-data">
+          <div className="cv-amount"># of CV's: 10</div>
+          <div className="jobs-amount"># of jobs applied: 50</div>
+        </div>
+      </div>
       <nav className="sidebar-nav">
-        <Link to="/home" className="sidebar-nav-item">Home</Link>
-        <Link to="/documents" className="sidebar-nav-item">Documents</Link>
-        <Link to="/job-search" className="sidebar-nav-item">Job Search</Link>
-        <Link to="/interview-prep" className="sidebar-nav-item">Interview Prep</Link>
-        <Link to="/job-search-strategy" className="sidebar-nav-item">Job Search Strategy</Link>
-        <Link to="/job-tracker" className="sidebar-nav-item">Job Tracker</Link>
-        <Link to="/career-insights" className="sidebar-nav-item">Career Insights</Link>
+        <Link to="/home" className="sidebar-nav-item">
+          <i className="fas fa-home"></i>
+          <span className="sidebar-nav-text">Home</span>
+        </Link>
+        <Link to="/cvs" className="sidebar-nav-item">
+          <i className="fas fa-file-alt"></i>
+          <span className="sidebar-nav-text">CV's</span>
+        </Link>
+        <Link to="/job-tracker" className="sidebar-nav-item">
+          <i className="fas fa-briefcase"></i>
+          <span className="sidebar-nav-text">Job Tracker</span>
+        </Link>
+        <Link to="/career-insights" className="sidebar-nav-item">
+          <i className="fas fa-chart-line"></i>
+          <span className="sidebar-nav-text">Career Insights</span>
+        </Link>
+        <Link to="/logout" className="sidebar-nav-item lo">
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="sidebar-nav-text">Log Out</span>
+        </Link>
       </nav>
     </div>
   );
